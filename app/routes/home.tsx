35cfb5fcccc7e7f09f7691038aff718c9ab0d96a
Osiego <cparker900@gmail.com>
2025-02-07 21:36:48 -0800

    addedpages

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,20 @@
-import type { Route } from "./+types/home";
+import type { RouterState } from "@remix-run/router";
 import { Welcome } from "../welcome/welcome";
+import { Page } from "../app/home/index.tsx";
 
-export function meta({}: Route.MetaArgs) {
+export function meta({}: { state: RouterState }) {
   return [
     { title: "New React Router App" },
     { name: "description", content: "Welcome to React Router!" },
+   
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return(
+  <div>
+  <Welcome />
+  <Page />
+</div>
+);
 }
